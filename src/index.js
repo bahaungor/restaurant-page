5286e693b2582fbdd9822783f5d6632863266322
Bahaungor <96170851+Bahaungor@users.noreply.github.com>
2022-04-04 17:51:30 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let loadPage = (function(){
+    const content = document.querySelector("#content");
+    content.textContent = "Bahaaaaa";
+    content.style.backgroundColor = "red";
+})();
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path'); 
+
+module.exports = {
+    entry: './src/index.js', 
+    devtool: 'inline-source-map', //It will be easier to track down errors 
+    output: { 
+        filename: 'main.js', 
+        path: path.resolve(__dirname, 'dist'), 
+        clean: true, // This will cleanup output folder (dist) after each build 
+    }, 
+}; 
